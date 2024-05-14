fe9e25fe575442c77e4b5c28a9913b6f6cd95dba
Prabhat <prabhat@Prabhats-Macbook.local>
2024-05-14 18:03:12 +0530

    app config update

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-	site_name: 'AstroPress',
-	title: 'AstroPress | Astro and Tailwind CSS',
-	description: 'Boilerplate built with Astro and Tailwind CSS',
-	author: 'Nicolas',
-	locale_region: 'de-en',
-	locale: 'en'
+  site_name: "prabhatkumar.dev",
+  title: "Prabhat Kumar",
+  description: "Personal Showcase Portfolio Website ",
+  author: "Prabhat",
+  locale_region: "de-en",
+  locale: "en",
 };
